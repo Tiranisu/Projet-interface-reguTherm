3fc061fb2f23f55f52510497b54ae9a1bd0e54ec
Mael-GN <mael.grelneau@gmail.com>
2023-03-20 15:00:07 +0100

    update

--- Src/regulation.c
+++ Src/regulation.c
@@ -2,10 +2,24 @@
 	 #include <stdlib.h>
     #include "../Inc/regulation.h"
 	 
+	 /**
+	  * @brief 
+	  * 
+	  * @param regul Si 1 la regulation est en TOR, si 2 la régulation est en PID
+	  * @param consigne Température de consigne
+	  * @param tabT Tableau de température
+	  * @param nT Taille du tableau de température
+	  * @return float 
+	  */
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
 		float cmd=0; // <---
-		
+
+		if(regul == 1){
+			if(tabT[nT-1] > consigne){
+				return 50;
+			}
+		}if(regul == 2){
+			
+		}
 		return cmd;
 	}
-    
-  
