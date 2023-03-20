a8b142999fadb9d536be87a0a7f10271af36e314
0nano <35236525+0nano@users.noreply.github.com>
2023-03-20 17:24:48 +0100

    Consigne ok

--- Inc/consigne.h
+++ Inc/consigne.h
@@ -1,8 +1,12 @@
+#ifndef CONSIGNE_H_
+#define CONSIGNE_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "define.h" 
 
 // Pensez à bien proteger vos .h avec des ifndef
 
-
 float consigne(float thermostatPrec_f);
+
+#endif
--- Src/consigne.c
+++ Src/consigne.c
@@ -4,6 +4,29 @@ float consigne(float thermostatPrec_f){
 
    float thermostat_f=0;
 
+   if(access(".verrouConsigne", F_OK) != 0){
+      
+      FILE* consigne = fopen("consigne.txt", "r");
    
+      if(!consigne){
+         perror("Un problème est survenu pendant la création/ouverture du fichier.");
+         return;
+      }
+
+      FILE* verrouConsigne = fopen(".verrouConsigne", "w");
+
+      if(!verrouConsigne){
+         perror("Un problème est survenu pendant la création/ouverture du fichier.");
+         return;
+      }
+
+      fscanf(consigne, "%f", &thermostat_f);
+      fclose(consigne);
+
+      remove(".verrouConsigne");
+   }else{
+      thermostat_f = thermostatPrec_f;
+   }
+
    return thermostat_f;
 }
